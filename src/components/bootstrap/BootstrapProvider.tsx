b9e4480f3491d1fab39496b6881b02697415798a
RAFAELLE HEMMEL OTSUKA <rafaelle.hotsuka@CAS474761C148A.local>
2025-03-24 20:20:34 -0300

    Projeto Buildado

--- src/components/bootstrap/BootstrapProvider.tsx
+++ src/components/bootstrap/BootstrapProvider.tsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function BootstrapProvider() {
   useEffect(() => {
+    // @ts-ignore
     import("bootstrap/dist/js/bootstrap.bundle.min")
   }, []);
 
